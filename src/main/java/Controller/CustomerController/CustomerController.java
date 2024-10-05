e6412246bc8fdc525d6afa96b63614664325c622
Chathur0 <145315287+Chathur0@users.noreply.github.com>
2024-10-05 22:40:01 +0530

    Completed Customer Form

--- src/main/java/Controller/CustomerController/CustomerController.java
+++ src/main/java/Controller/CustomerController/CustomerController.java
@@ -5,7 +5,6 @@ import db.DBConnection;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -32,17 +31,55 @@ public class CustomerController {
         }
         return allCustomer;
     }
-    public static int addCustomer(Customer customer) throws SQLException {
+
+    public static boolean addCustomer(Customer customer) throws SQLException {
         PreparedStatement stmt = DBConnection.getInstance().getConnection().prepareStatement("Insert into customer values(?,?,?,?,?,?,?,?,?)");
-        stmt.setObject(1,customer.getId());
-        stmt.setObject(2,customer.getTitle());
-        stmt.setObject(3,customer.getName());
-        stmt.setObject(4,customer.getDob());
-        stmt.setObject(5,customer.getSalary());
-        stmt.setObject(6,customer.getAddress());
-        stmt.setObject(7,customer.getCity());
-        stmt.setObject(8,customer.getProvince());
-        stmt.setObject(9,customer.getPostalCode());
-        return stmt.executeUpdate();
+        stmt.setObject(1, customer.getId());
+        stmt.setObject(2, customer.getTitle());
+        stmt.setObject(3, customer.getName());
+        stmt.setObject(4, customer.getDob());
+        stmt.setObject(5, customer.getSalary());
+        stmt.setObject(6, customer.getAddress());
+        stmt.setObject(7, customer.getCity());
+        stmt.setObject(8, customer.getProvince());
+        stmt.setObject(9, customer.getPostalCode());
+        return stmt.executeUpdate() > 0;
+    }
+
+    public static boolean deleteCustomer(String customerID) throws SQLException {
+        PreparedStatement stmt = DBConnection.getInstance().getConnection().prepareStatement("Delete from customer where CustID=?");
+        stmt.setObject(1, customerID);
+        return stmt.executeUpdate() > 0;
+    }
+
+    public static Customer getCustomer(String customerID) throws SQLException {
+        PreparedStatement stmt = DBConnection.getInstance().getConnection().prepareStatement("Select * from customer where CustID=?");
+        stmt.setObject(1, customerID);
+        ResultSet resultSet = stmt.executeQuery();
+        return resultSet.next() ? new Customer(
+                resultSet.getString(1),
+                resultSet.getString(2),
+                resultSet.getString(3),
+                resultSet.getDate(4).toLocalDate(),
+                resultSet.getDouble(5),
+                resultSet.getString(6),
+                resultSet.getString(7),
+                resultSet.getString(8),
+                resultSet.getString(9)
+        ) : null;
+    }
+
+    public static boolean updateCustomer(Customer customer) throws SQLException {
+        PreparedStatement stmt = DBConnection.getInstance().getConnection().prepareStatement("Update customer set CustTitle=?, CustName=?, DOB=?, salary=?, CustAddress=?, City=?, Province=?, PostalCode=? where CustID=?");
+        stmt.setObject(9, customer.getId());
+        stmt.setObject(1, customer.getTitle());
+        stmt.setObject(2, customer.getName());
+        stmt.setObject(3, customer.getDob());
+        stmt.setObject(4, customer.getSalary());
+        stmt.setObject(5, customer.getAddress());
+        stmt.setObject(6, customer.getCity());
+        stmt.setObject(7, customer.getProvince());
+        stmt.setObject(8, customer.getPostalCode());
+        return stmt.executeUpdate() > 0;
     }
 }
--- src/main/java/Controller/CustomerController/CustomerFormController.java
+++ src/main/java/Controller/CustomerController/CustomerFormController.java
@@ -4,13 +4,12 @@ import Model.Customer;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
-import db.DBConnection;
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableArray;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -82,31 +81,98 @@ public class CustomerFormController implements Initializable {
 
     @FXML
     void btnAdd(ActionEvent event) {
-        try {
-            int isAdded = CustomerController.addCustomer(new Customer(txtCustId.getText().toUpperCase(), cmbTitle.getValue(), txtCustName.getText(), dPDOB.getValue(), Double.parseDouble(txtSalary.getText()), txtAddress.getText(), txtCity.getText(), txtProvince.getText(), txtPosCode.getText()));
-            if (isAdded > 0) {
-                JOptionPane.showMessageDialog(null, "Customer Added SuccessFully", "Success", JOptionPane.INFORMATION_MESSAGE);
-            } else {
-                JOptionPane.showMessageDialog(null, "Customer Added Unsuccessfully", "Error", JOptionPane.ERROR_MESSAGE);
+        if (!txtCustId.getText().isEmpty() && cmbTitle.getValue() != null && !txtCustName.getText().isEmpty() && dPDOB.getValue() != null && !txtSalary.getText().isEmpty() && !txtAddress.getText().isEmpty() && !txtCity.getText().isEmpty() && !txtProvince.getText().isEmpty() && !txtPosCode.getText().isEmpty()) {
+            try {
+                boolean isAdded = CustomerController.addCustomer(new Customer(
+                        txtCustId.getText().toUpperCase(),
+                        cmbTitle.getValue(),
+                        txtCustName.getText(),
+                        dPDOB.getValue(),
+                        Double.parseDouble(txtSalary.getText()),
+                        txtAddress.getText(),
+                        txtCity.getText(),
+                        txtProvince.getText(),
+                        txtPosCode.getText()
+                ));
+                if (isAdded) {
+                    JOptionPane.showMessageDialog(null, "Customer Added SuccessFully", "Success", JOptionPane.INFORMATION_MESSAGE);
+                    clearFields();
+                } else {
+                    JOptionPane.showMessageDialog(null, "Customer Added Unsuccessfully", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+            } catch (SQLException e) {
+                JOptionPane.showMessageDialog(null, "Error Added Customer due to SQL Error");
             }
-        } catch (SQLException e) {
-            JOptionPane.showMessageDialog(null, "Error Added Customer due to SQL Error");
+        } else {
+            JOptionPane.showMessageDialog(null, "Please fill all fields");
         }
     }
 
     @FXML
     void btnDelete(ActionEvent event) {
-
+        if (!txtCustId.getText().isEmpty()) {
+            try {
+                if (CustomerController.deleteCustomer(txtCustId.getText())) {
+                    new Alert(Alert.AlertType.INFORMATION, txtCustId.getText() + " Customer Deleted").show();
+                    clearFields();
+                    loadTable();
+                } else {
+                    new Alert(Alert.AlertType.ERROR, "Customer Not Found").show();
+                }
+            } catch (SQLException e) {
+                new Alert(Alert.AlertType.ERROR, "Customer Not Found or MYSQL Sever Error").show();
+            }
+        } else {
+            new Alert(Alert.AlertType.ERROR, "Customer Not Selected").show();
+        }
     }
 
     @FXML
     void btnSearch(ActionEvent event) {
-
+        if (!txtCustId.getText().isEmpty()) {
+            try {
+                Customer customer = CustomerController.getCustomer(txtCustId.getText());
+                if (customer != null) {
+                    setTxtFields(customer);
+                } else {
+                    new Alert(Alert.AlertType.ERROR, "Customer not found").show();
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            new Alert(Alert.AlertType.ERROR, "Customer not Selected").show();
+        }
     }
 
     @FXML
     void btnUpdate(ActionEvent event) {
-
+        if (!txtCustId.getText().isEmpty() && cmbTitle.getValue() != null && !txtCustName.getText().isEmpty() && dPDOB.getValue() != null && !txtSalary.getText().isEmpty() && !txtAddress.getText().isEmpty() && !txtCity.getText().isEmpty() && !txtProvince.getText().isEmpty() && !txtPosCode.getText().isEmpty()) {
+            try {
+                boolean isUpdated = CustomerController.updateCustomer(new Customer(
+                        txtCustId.getText().toUpperCase(),
+                        cmbTitle.getValue(),
+                        txtCustName.getText(),
+                        dPDOB.getValue(),
+                        Double.parseDouble(txtSalary.getText()),
+                        txtAddress.getText(),
+                        txtCity.getText(),
+                        txtProvince.getText(),
+                        txtPosCode.getText()
+                ));
+                if (isUpdated) {
+                    JOptionPane.showMessageDialog(null, "Customer Updated SuccessFully", "Success", JOptionPane.INFORMATION_MESSAGE);
+                    clearFields();
+                    loadTable();
+                } else {
+                    JOptionPane.showMessageDialog(null, "Customer Updated Unsuccessfully Customer Not Found", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            JOptionPane.showMessageDialog(null, "Please fill all fields");
+        }
     }
 
     public void btnOnActionReload(ActionEvent actionEvent) {
@@ -178,11 +244,48 @@ public class CustomerFormController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         btnReload.fire(); // this fire() will act like manual button click, so it will execute "btnOnActionReload" function
+        setTitle();
+        tableSelectionChange();
+    }
+
+    private void setTitle() {
         ObservableList<String> title = FXCollections.observableArrayList();
         title.add("Mr");
         title.add("Mrs");
         title.add("Miss");
         title.add("Ms");
         cmbTitle.setItems(title);
+    }
+
+    private void tableSelectionChange() {
+        tblCustomer.getSelectionModel().selectedItemProperty().addListener((observableValue, number, selectedTableValue) -> {
+            if (selectedTableValue != null) {
+                setTxtFields(selectedTableValue);
+            }
+        });
+    }
+
+    private void clearFields() {
+        txtCustId.setText("");
+        cmbTitle.setValue(null);
+        txtCustName.setText("");
+        dPDOB.setValue(null);
+        txtSalary.setText("");
+        txtAddress.setText("");
+        txtCity.setText("");
+        txtProvince.setText("");
+        txtPosCode.setText("");
+    }
+
+    private void setTxtFields(Customer customer) {
+        txtCustId.setText(customer.getId());
+        cmbTitle.setValue(customer.getTitle());
+        txtCustName.setText(customer.getName());
+        dPDOB.setValue(customer.getDob());
+        txtSalary.setText(customer.getSalary() + "");
+        txtAddress.setText(customer.getAddress());
+        txtCity.setText(customer.getCity());
+        txtProvince.setText(customer.getProvince());
+        txtPosCode.setText(customer.getPostalCode());
     }
 }
